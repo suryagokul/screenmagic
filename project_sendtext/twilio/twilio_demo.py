c82b38d96b3dbe981a41c702ab7d2b98589dfc6b
Surya <41636357+suryagokul@users.noreply.github.com>
2021-05-15 18:46:18 +0530

    Update twilio_demo.py

--- project_sendtext/twilio/twilio_demo.py
+++ project_sendtext/twilio/twilio_demo.py
@@ -1,24 +1,11 @@
 from twilio.rest import Client
 
-'''
-create twilio_keys file in same directory. 
-	
-	In that file type below line & replace values with ur api keys. 
-	
-		account_sid, auth_token, mobile_no = "urid", "urtoken", "receiverno"
-
-'''
-
 #importing credentials from created file
 from twilio_keys import account_sid, auth_token,mobile_no
 
 
 # paste account sid and auth token
 client = Client(account_sid, auth_token)
-
-# change the "from_" number to your Twilio number and the "to" number
-# to the phone number you signed up for Twilio with, or upgrade your
-# account to send SMS to any phone number
 
 client.messages.create(to=mobile_no, 
                        from_="+17606153242", 
