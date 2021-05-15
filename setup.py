c1f29a497377b1ef4ca727ddd7cce195db1564c9
Surya <41636357+suryagokul@users.noreply.github.com>
2021-05-15 19:12:44 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'scheduling mails and messages'
+LONG_DESCRIPTION = 'A package that allows to take data like message,mobileno, email and scheduleon from csv file and send mails,text messages.'
+
+# Setting up
+setup(
+    name="Sendmails&messages",
+    version=VERSION,
+    author="NeuralNine (Florian Dedov)",
+    author_email="<peddintigokul946@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
